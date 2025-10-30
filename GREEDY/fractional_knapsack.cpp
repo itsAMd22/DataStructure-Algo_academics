cd7ad146faa9f31d3ec2ce7e4fd1aba4485958b0
Mohammed Arif <mo.arif.2205@gmail.com>
2025-10-30 21:20:44 +0600

    Update fractional_knapsack.cpp

--- GREEDY/fractional_knapsack.cpp
+++ GREEDY/fractional_knapsack.cpp
@@ -1,6 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void SelectionSort(int n, int profit[], int weight[], float Ratio[]);
+void Merge(int profit[], int weight[], float Ratio[], int l, int mid, int r);
+void MergeSort(int profit[], int weight[], float Ratio[], int l, int r);
+
+void fknapsack(int n, int m, int profit[], int weight[], float Ratio[]){
+    float X[n] = {0.0}, t_profit = 0, left = m;
+    int i = 0;
+
+    while(i < n){
+        if(weight[i] <= left){
+            left -= weight[i], X[i] = 1.0;
+            t_profit += profit[i];
+        }else{
+            break;
+        }
+        i++;
+    }
+
+    if(i < n){
+        X[i] = left / weight[i];
+        t_profit += profit[i] * X[i], left -= X[i] * weight[i];
+    }
+    
+    cout << "Sack weight = " << m - left << '\n';
+    cout << "Total profit = " << t_profit << '\n';
+}
+
+void solve(){
+    int n, m;
+    cin >> n >> m;
+    int profit[n], weight[n];
+    float Ratio[n];
+
+    for(int i = 0 ; i < n ; i++){
+        cin >> profit[i] >> weight[i];
+        Ratio[i] = (1.0 * profit[i]) / weight[i];
+    }
+    // SelectionSort(n, profit, weight, Ratio); // results in O(n^2) complexity for the whole code.
+    MergeSort(profit, weight, Ratio, 0, n - 1); // results in O(n logn) complexity for the whole code.
+    
+    fknapsack(n, m, profit, weight, Ratio);
+}
+
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+
 void SelectionSort(int n, int profit[], int weight[], float Ratio[]){
     // O(n ^ 2)
     int j;
@@ -61,53 +116,3 @@ void MergeSort(int profit[], int weight[], float Ratio[], int l, int r){
     }
 }
 
-void fknapsack(int n, int m, int profit[], int weight[], float Ratio[]){
-    float X[n] = {0.0}, t_profit = 0, left = m;
-    int i = 0;
-
-    while(i < n){
-        if(weight[i] <= left){
-            left -= weight[i], X[i] = 1.0;
-            t_profit += profit[i];
-        }else{
-            break;
-        }
-        i++;
-    }
-
-    if(i < n){
-        X[i] = left / weight[i];
-        t_profit += profit[i] * X[i], left -= X[i] * weight[i];
-    }
-    
-    cout << "Sack weight = " << m - left << '\n';
-    cout << "Total profit = " << t_profit << '\n';
-}
-
-void solve(){
-    int n, m;
-    cin >> n >> m;
-    int profit[n], weight[n];
-    float Ratio[n];
-
-    for(int i = 0 ; i < n ; i++){
-        cin >> profit[i] >> weight[i];
-        Ratio[i] = (1.0 * profit[i]) / weight[i];
-    }
-    
-
-    // SelectionSort(n, profit, weight, Ratio); // results in O(n^2) complexity for the whole code.
-    MergeSort(profit, weight, Ratio, 0, n - 1); // results in O(n logn) complexity for the whole code.
-    
-    fknapsack(n, m, profit, weight, Ratio);
-}
-
-int main(){
-    ios_base::sync_with_stdio(0), cin.tie(0);
-    int t = 1;
-    // cin >> t;
-    while(t--){
-        solve();
-    }
-    return 0;
-}
