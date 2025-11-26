8914f949c5601efe2b4aadd30dacc4c4444fa063
Mohammed Arif <mo.arif.2205@gmail.com>
2025-11-26 23:21:21 +0600

    Update 2.0_1_knapsack.cpp

--- GREEDY/2.0_1_knapsack.cpp
+++ GREEDY/2.0_1_knapsack.cpp
@@ -51,9 +51,10 @@ void solve(){
 int main(){
     ios_base::sync_with_stdio(0), cin.tie(0);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while(t--){
         solve();
     }
     return 0;
+
 }
