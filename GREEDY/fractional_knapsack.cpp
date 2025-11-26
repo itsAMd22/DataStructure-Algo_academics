480fae1454e4e9676a8b75d0d4463513e72afda8
Mohammed Arif <mo.arif.2205@gmail.com>
2025-11-26 23:14:34 +0600

    Update fractional_knapsack.cpp

--- GREEDY/fractional_knapsack.cpp
+++ GREEDY/fractional_knapsack.cpp
@@ -1,118 +1,96 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void SelectionSort(int n, int profit[], int weight[], float Ratio[]);
-void Merge(int profit[], int weight[], float Ratio[], int l, int mid, int r);
-void MergeSort(int profit[], int weight[], float Ratio[], int l, int r);
+void MergeArray(int l, int mid, int r, int profit[], int weight[], float ratio[]){
+    int i = l, k = l, j = mid + 1, ex1[r + 1], ex2[r + 1];
+    float ex3[r + 1];
 
-void fknapsack(int n, int m, int profit[], int weight[], float Ratio[]){
-    float X[n] = {0.0}, t_profit = 0, left = m;
-    int i = 0;
-
-    while(i < n){
-        if(weight[i] <= left){
-            left -= weight[i], X[i] = 1.0;
-            t_profit += profit[i];
+    while(i <= mid && j <= r){
+        if((ratio[i] > ratio[j]) || (ratio[i] == ratio[j] && profit[i] >= profit[j])){
+            ex1[k] = profit[i], ex2[k] = weight[i], ex3[k] = ratio[i];
+            i++;
         }else{
-            break;
+            ex1[k] = profit[j], ex2[k] = weight[j], ex3[k] = ratio[j];
+            j++;
         }
+        k++;
+    }
+
+    while(i <= mid){
+        ex1[k] = profit[i], ex2[k] = weight[i], ex3[k] = ratio[i];
+        i++, k++;
+    }
+
+    while(j <= r){
+        ex1[k] = profit[j], ex2[k] = weight[j], ex3[k] = ratio[j];
+        j++, k++;
+    }
+
+    for(int p = l ; p <= r ; p++){
+        profit[p] = ex1[p], weight[p] = ex2[p], ratio[p] = ex3[p];
+    }
+}
+
+
+void mergeSort(int l, int r, int profit[], int weight[], float ratio[]){
+    if(l < r){
+        int mid = (l + r) / 2;
+        mergeSort(l, mid, profit, weight, ratio);
+        mergeSort(mid + 1, r, profit, weight, ratio);
+        MergeArray(l, mid, r, profit, weight, ratio);
+    }
+}
+
+
+void fknapsack(int n, int m, int profit[], int weight[], float ratio[]){
+    int i = 1, W = m;
+    float prof = 0.0, X[n + 1] = {0.0};
+    
+    while(i <= n && m > 0){
+        X[i] = min(1.0, 1.0 * m / weight[i]);
+        m -= (X[i] * weight[i]);
         i++;
     }
 
-    if(i < n){
-        X[i] = left / weight[i];
-        t_profit += profit[i] * X[i], left -= X[i] * weight[i];
+    for(int i = 1 ; i <= n ; i++){
+        prof += (X[i] * profit[i]);
     }
-    
-    cout << "Sack weight = " << m - left << '\n';
-    cout << "Total profit = " << t_profit << '\n';
+
+    cout << "\nMax profit = " << prof << '\n';
+    cout << "Sack weight = " << W - m << '\n';
 }
 
 void solve(){
-    int n, m;
+    int n;
+    float m;
     cin >> n >> m;
-    int profit[n], weight[n];
-    float Ratio[n];
 
-    for(int i = 0 ; i < n ; i++){
+    int profit[n + 1], weight[n + 1];
+    float ratio[n + 1];
+
+    for(int i = 1 ; i <= n ; i++){
         cin >> profit[i] >> weight[i];
-        Ratio[i] = (1.0 * profit[i]) / weight[i];
+        ratio[i] = 1.0 * profit[i] / weight[i];
     }
-    // SelectionSort(n, profit, weight, Ratio); // results in O(n^2) complexity for the whole code.
-    MergeSort(profit, weight, Ratio, 0, n - 1); // results in O(n logn) complexity for the whole code.
-    
-    fknapsack(n, m, profit, weight, Ratio);
+        
+    mergeSort(1, n, profit, weight, ratio);
+
+    // cout << "Profit : ";
+    // for(int i = 1 ; i <= n ; i++)   cout << profit[i] << " "; cout << endl;
+    // cout << "Weight : ";
+    // for(int i = 1 ; i <= n ; i++)   cout << weight[i] << " "; cout << endl;
+    // cout << "Ratio : ";
+    // for(int i = 1 ; i <= n ; i++)   cout << ratio[i] << " "; cout << endl;
+
+    fknapsack(n, m, profit, weight, ratio);
 }
 
 int main(){
     ios_base::sync_with_stdio(0), cin.tie(0);
     int t = 1;
-    // cin >> t;
+    cin >> t;
     while(t--){
         solve();
     }
     return 0;
 }
-
-
-
-void SelectionSort(int n, int profit[], int weight[], float Ratio[]){
-    // O(n ^ 2)
-    int j;
-    for(int i = 0 ; i < n ; i++){
-        j = i + 1;
-        while(j < n){
-            if(Ratio[i] < Ratio[j] || (Ratio[i] == Ratio[j] && profit[i] < profit[j])){
-                swap(profit[i], profit[j]); 
-                swap(weight[i], weight[j]);
-                swap(Ratio[i], Ratio[j]);
-            }
-            j++;
-        }
-    }
-}
-
-void Merge(int profit[], int weight[], float Ratio[], int l, int mid, int r){
-    int i, j, k, c1[r + 1], c2[r + 1];
-    float c3[r + 1];
-
-    i = k = l, j = mid + 1;
-
-    while(i <= mid && j <= r){
-        if(Ratio[i] > Ratio[j]){
-            c1[k] = profit[i], c2[k] = weight[i], c3[k] = Ratio[i];
-            i++, k++;
-        }else if(Ratio[i] == Ratio[j] && profit[i] > profit[j]){
-            c1[k] = profit[i], c2[k] = weight[i], c3[k] = Ratio[i];
-            i++, k++;
-        }else{
-            c1[k] = profit[j], c2[k] = weight[j], c3[k] = Ratio[j];
-            j++, k++;
-        }
-    }
-
-    while(i <= mid){
-        c1[k] = profit[i], c2[k] = weight[i], c3[k] = Ratio[i];
-        i++, k++;
-    }
-
-    while(j <= r){
-        c1[k] = profit[j], c2[k] = weight[j], c3[k] = Ratio[j];
-        j++, k++;
-    }
-
-    for(int i = l ; i <= r ; i++){
-        profit[i] = c1[i], weight[i] = c2[i], Ratio[i] = c3[i];
-    }
-}
-
-void MergeSort(int profit[], int weight[], float Ratio[], int l, int r){
-    //O (n log n)
-    if(l < r){
-        int mid = (l + r) / 2;
-        MergeSort(profit, weight, Ratio, l, mid);
-        MergeSort(profit, weight, Ratio, mid + 1, r);
-        Merge(profit, weight, Ratio, l, mid, r);
-    }
-}
-
