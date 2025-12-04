b0d5883631e2c5694b8473f2bd5ae26d15329931
Mohammed Arif <mo.arif.2205@gmail.com>
2025-12-04 22:33:19 +0600

    Add files via upload

---
+++ DP/1.2.Fibonacci_Tabulation.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MX = 1e6;
+int memo[MX];
+
+int fib(int n){
+    if(n <= 1){
+        return n;
+    }  
+    int fib[n];
+    fib[0] = 0, fib[1] = 1;
+    for(int i = 2 ; i <= n ; i++){
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return fib[n];
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fib(n) << '\n';
+    return 0;
+}
---
+++ DP/2.2.Factorial_Tabulation.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    if(n <= 1){
+        return 1;
+    }  
+    int fact[n];
+    fact[0] = 1, fact[1] = 1;
+    for(int i = 2 ; i <= n ; i++){
+        fact[i] = i * fact[i - 1];
+    }
+    return fact[n];
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fact(n) << '\n';
+    return 0;
+}
