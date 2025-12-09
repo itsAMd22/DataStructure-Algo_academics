e507998e7467248f6a91cc89d66e9e569b7b4dab
Mohammed Arif <mo.arif.2205@gmail.com>
2025-12-09 19:35:21 +0600

    Update 3.2. 0_1_knapsack_Memo.cpp

--- DP/3.2. 0_1_knapsack_Memo.cpp
+++ DP/3.2. 0_1_knapsack_Memo.cpp
@@ -1,59 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int MX = 1e3 + 5;
-int dp[MX][MX];
+const int mx = 1e3 + 5;
+int n, m, memo[mx][mx];
 
-int knap(int i, int available_W, int profit[], int weight[]){
-    int j = available_W;
+int knap(int i, int avW, int p[], int w[]){
+    if(i > n || avW <= 0)   return 0;
+    if(memo[i][avW] != -1)  return memo[i][avW];
+    if(w[i] > avW)  return memo[i][avW] = knap(i + 1, avW, p, w);
     
-    if(i < 1 || available_W <= 0)   return 0;   // not valid or not enough space in the sack.
-    if(dp[i][j] != -1)              return dp[i][j];    // already calculated
-    if(weight[i] > j)               return dp[i][j] = knap(i - 1, available_W, profit, weight);     // this weight cannot be taken
-    
-    // include
-    int inc = profit[i] + knap(i - 1, available_W - weight[i], profit, weight);
-
-    // exclude
-    int exc = knap(i - 1, available_W, profit, weight);
-    
-    return dp[i][j] = max(inc, exc);
+    int inc = p[i] + knap(i + 1, avW - w[i], p, w), exc = knap(i + 1, avW, p, w);
+    return memo[i][avW] = max(inc, exc);
 }
 
 int main(){
-    int n, m;
     cin >> n >> m;
 
     // dp table initialization
     for(int i = 0 ; i <= n ; i++){
         for(int j = 0 ; j <= m ; j++){
-            dp[i][j] = -1;
+            memo[i][j] = -1;
         }
     }
-
-
+    
     int profit[n + 1], weight[n + 1];
-
     for(int i = 1 ; i <= n ; i++){
         cin >> profit[i] >> weight[i];
     }    
     
-    cout << "\nMaximum profit = " << knap(n , m, profit, weight) << '\n';
+    cout << "\nMaximum profit = " << knap(1 , m, profit, weight) << '\n';
     
+    //print dp table
     cout << "\nMemorisation table :\n";
     for(int i = 0 ; i <= n ; i++){
         for(int j = 0 ; j <= m ; j++){
-            cout << dp[i][j] << " ";
+            cout << memo[i][j] << " ";
         }
         cout << '\n';
     }
 
     
 
-    int i = n, j = m, X[n + 1] = {0};
-    while(i > 0 && j > 0){
-        if(dp[i][j] != dp[i - 1][j])    X[i] = 1, j -= weight[i];
-        i--;
+    int i = 1, j = m, X[n + 1] = {0};
+    while(j > 0){
+        if(memo[i][j] != memo[i + 1][j])    X[i] = 1, j -= weight[i];
+        i++;
     }
 
     cout << "\nWeights taken : ";
@@ -63,5 +54,4 @@ int main(){
     cout << '\n';
 
     return 0;
-
 }
